0a2e7af7138dce110281dd96d4f9d340bb5d1f21
benjhamincoca <benjhamin.coca@ucb.edu.bo>
2025-04-10 09:49:19 -0400

    Add files via upload

---
+++ Practica en clase.py
@@ -0,0 +1,21 @@
+#resta de numeros
+
+Y = 2
+X = 3
+resta = Y - X
+print(Y - X)
+
+#Multiplicacion de numeros
+
+base = 2
+altura = 3
+multiplicacion = base * altura
+print(multiplicacion)
+
+#Division de numeros
+
+num1 = 2
+num2 = 2
+division = num1 / num2
+print(division)
+
